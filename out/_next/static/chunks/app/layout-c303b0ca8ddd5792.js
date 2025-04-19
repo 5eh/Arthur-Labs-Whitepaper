5df4474fada996b229898dccd86efa2d6b16437f
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2025-04-19 11:16:18 -0500

    :zap: Changed Whitepaper :zap:

--- out/_next/static/chunks/app/layout-c303b0ca8ddd5792.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7881:()=>{},9705:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7881,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[574,773,261,358],()=>s(9705)),_N_E=e.O()}]);
